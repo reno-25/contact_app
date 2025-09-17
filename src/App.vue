7e117321f080f0523e68d7038ccba377c04e2aae
reno <ryadimoreno@gmail.com>
2025-09-17 08:36:58 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <AppHeader title="Contact App"/>
+  <router-view></router-view>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'  
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style>
+
+</style>
